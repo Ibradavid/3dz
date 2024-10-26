2eea991d80175a35de72d4252c981b4a45575df4
Ibradavid <Ibraimovdavid0@gmail.com>
2024-10-26 16:37:18 +0600

    first commit

---
+++ apps/catalog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Cтроковое поле для имени автора")
+    birthdate = models.DateField(verbose_name="Поле даты для дня рождения автора")
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='genres')
+
+    def __str__(self):
+        return self.name
---
+++ apps/catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AuthorViewSet, BookViewSet, GenreViewSet
+
+router = DefaultRouter()
+router.register(r'authors', AuthorViewSet)
+router.register(r'books', BookViewSet)
+router.register(r'genres', GenreViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
